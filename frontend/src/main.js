64cf14993a4f1f4fd7e7f61963be1dc32e25f7d7
Louis Ramos <louisramosdev@gmail.com>
2023-04-22 23:53:28 +0200

    Installed fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,18 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
-import router from "./router/router"
-
-import App from "./App.vue"
-import useSessionStore from "./stores/SessionStore"
 
 import "./assets/stylesheets/style.scss"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+import { faEyeSlash, faEye } from "@fortawesome/free-solid-svg-icons"
+
+import App from "./App.vue"
+import router from "./router/router"
+import useSessionStore from "./stores/SessionStore"
 
 const app = createApp(App)
 const pinia = createPinia()
+library.add(faEyeSlash, faEye)
 
 // Load JWT from local storage on refresh
 
@@ -19,4 +23,8 @@ if (authTokenExists) {
   await useSessionStore(pinia).loginUserWithToken(authToken)
 }
 
-app.use(router).use(pinia).mount("#app")
+app
+  .component("FontAwesomeIcon", FontAwesomeIcon)
+  .use(router)
+  .use(pinia)
+  .mount("#app")
