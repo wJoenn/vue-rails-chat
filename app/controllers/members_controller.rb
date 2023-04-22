9e5d8089c844c6cf3c6b3c51f731409679ffe729
Louis Ramos <louisramosdev@gmail.com>
2023-04-22 18:35:24 +0200

    Added jwt to denylist if no corrsponding user

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,20 +1,34 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!
-
   def show
     user = set_user_from_token
 
-    render json: { message: "If you see this you're logged in", user: }
+    if user
+      render json: { message: "If you see this you're logged in", user: }
+    else
+      jwt_payload = set_jwt_payload
+      jti = jwt_payload["jti"]
+      exp = Time.zone.at(jwt_payload["exp"]).to_datetime
+
+      JwtDenylist.create(jti:, exp:)
+
+      render json: {
+        message: "Your JWT token was linked to a deleted user account"
+      }, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_user_from_token
-    jwt_payload = JWT.decode(request.headers["Authorization"].split[1],
-      Rails.application.credentials.devise[:jwt_secret_key]).first
+    jwt_payload = set_jwt_payload
 
     user_id = jwt_payload["sub"]
 
-    User.find(user_id)
+    User.find_by(id: user_id)
+  end
+
+  def set_jwt_payload
+    JWT.decode(request.headers["Authorization"].split[1],
+      Rails.application.credentials.devise[:jwt_secret_key]).first
   end
 end
