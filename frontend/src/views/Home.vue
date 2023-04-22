288d2421293c7ae9c3495012870842467e24fc4c
Louis Ramos <louisramosdev@gmail.com>
2023-04-22 18:38:25 +0200

    Added forms and button for user sessions and registrations

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="home">
+    <h1>Hello {{ sessionStore.getUserEmail }}</h1>
+
     <div>
       <a href="https://vitejs.dev" target="_blank">
         <img src="../assets/images/vite.svg" class="logo" alt="Vite logo">
@@ -8,14 +10,65 @@
       <a href="https://vuejs.org/" target="_blank">
         <img src="../assets/images/vue.svg" class="logo vue" alt="Vue logo">
       </a>
+
+      <a href="https://pinia.vuejs.org/" target="_blank">
+        <img src="../assets/images/pinia.svg" class="logo pinia" alt="Vue logo">
+      </a>
     </div>
 
     <HelloWorld msg="Vite + Vue" />
+
+    <button @click="sessionStore.logoutUser()">Log out</button>
+
+    <div>
+      <h2>Sign In</h2>
+      <form @submit.prevent="signIn">
+        <input v-model="signInEmail" type="text" placeholder="email">
+        <input v-model="signInPassword" type="password" placeholder="password">
+        <button>Sign in</button>
+      </form>
+
+      <h2>Sign Up</h2>
+      <form @submit.prevent="signUp">
+        <input v-model="signUpEmail" type="text" placeholder="email">
+        <input v-model="signUpPassword" type="password" placeholder="password">
+        <button>Sign up</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script setup>
+  import { ref } from "vue"
   import HelloWorld from "../components/HelloWorld.vue"
+  import useSessionStore from "../stores/SessionStore"
+
+  const sessionStore = useSessionStore()
+  const signInEmail = ref("")
+  const signInPassword = ref("")
+  const signUpEmail = ref("")
+  const signUpPassword = ref("")
+
+  const resetRef = () => {
+    signInEmail.value = ""
+    signInPassword.value = ""
+    signUpEmail.value = ""
+    signUpPassword.value = ""
+  }
+
+  const signIn = async () => {
+    const params = { email: signInEmail.value, password: signInPassword.value }
+
+    const isSignedIn = await sessionStore.loginUser(params)
+    if (isSignedIn) resetRef()
+  }
+
+  const signUp = async () => {
+    const params = { email: signUpEmail.value, password: signUpPassword.value }
+
+    const isRegistered = await sessionStore.registerUser(params)
+    if (isRegistered) resetRef()
+  }
 </script>
 
 <style lang="scss" scoped>
@@ -26,6 +79,10 @@
 
     &:hover {
       filter: drop-shadow(0 0 2em #646cffaa);
+    }
+
+    &.pinia:hover {
+      filter: drop-shadow(0 0 2em #ffe368aa);
     }
 
     &.vue:hover {
