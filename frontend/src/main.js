ff39d36bf980be56115456d87eeffb8946907459
Louis Ramos <louisramosdev@gmail.com>
2023-04-22 14:33:34 +0200

    Added auth verification before mounting app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,22 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
 import router from "./router/router"
+
 import App from "./App.vue"
+import useSessionStore from "./stores/SessionStore"
 
 import "./assets/stylesheets/style.scss"
 
 const app = createApp(App)
 const pinia = createPinia()
+
+// Load JWT from local storage on refresh
+
+const authToken = localStorage.getItem("auth_token")
+const authTokenExists = authToken !== "undefined" && authToken !== null
+
+if (authTokenExists) {
+  await useSessionStore.loginUserWithToken(authToken)
+}
 
 app.use(router).use(pinia).mount("#app")
