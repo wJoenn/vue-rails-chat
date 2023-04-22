54fdd8fe6948ac4f47ed7a15f0365de218a9e6bf
Louis Ramos <louisramosdev@gmail.com>
2023-04-22 14:33:13 +0200

    Created SessionStore actions

--- frontend/src/stores/SessionStore.js
+++ frontend/src/stores/SessionStore.js
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia"
 
+const API_URL = "http://localhost:3000"
+
 const useSessionStore = defineStore("SessionStore", {
   state: () => ({
     auth_token: null,
@@ -9,6 +11,7 @@ const useSessionStore = defineStore("SessionStore", {
       username: null
     }
   }),
+
   getters: {
     getAuthToken() {
       return this.auth_token
@@ -30,6 +33,55 @@ const useSessionStore = defineStore("SessionStore", {
       const loggedOut = this.auth_token === null || this.auth_token === JSON.stringify(null)
 
       return !loggedOut
+    }
+  },
+
+  actions: {
+    async registerUser(payload) {
+      const res = await fetch(`${API_URL}/users`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ payload })
+      })
+
+      if (!res.ok) {
+        throw new Error(`HTTP error ${res.status}`);
+      }
+
+      console.log(res.json())
+    },
+
+    async loginUser(payload) {
+      const res = await fetch(`${API_URL}/users/sign_in`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ payload })
+      })
+
+      if (!res.ok) {
+        throw new Error(`HTTP error ${res.status}`);
+      }
+      console.log(res.json())
+    },
+
+    async loginUserWithToken(payload) {
+      const res = await fetch(`${API_URL}/member-data`, {
+        headers: { authorization: payload.auth_token }
+      })
+
+      console.log(res.json())
+    },
+
+    async logoutUser() {
+      const res = await fetch(`${API_URL}/users/sign_out`, {
+        method: "DELETE",
+        headers: { authorization: this.auth_token }
+      })
+
+      if (!res.ok) {
+        throw new Error(`HTTP error ${res.status}`);
+      }
+      console.log(res.json())
     }
   }
 })
