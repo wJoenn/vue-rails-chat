4ef964a0ec7057b2f59fe26685978070e960e643
Louis Ramos <louisramosdev@gmail.com>
2023-04-23 18:12:48 +0200

    added user email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
     :validatable,
     :jwt_authenticatable,
     jwt_revocation_strategy: JwtDenylist
+
+  validates :email, format: { with: /\A[\w\d]+(?:[._-]?[\w\d]+)*@[\w\d]+(?:[.-]?[\w\d]+)*(?:\.\w{2,})+\z/ }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "#create" do
     it "creates a new user" do
-      user = User.create(email: "a@a.a", password: "aaaaaa")
+      user = User.create(email: "a@a.ab", password: "aaaaaa")
 
       expect(user.persisted?).to eq(true)
     end
@@ -19,22 +19,22 @@ RSpec.describe User, type: :model do
     end
 
     it "requires a valid password" do
-      user_one = User.create(email: "a@a.a")
-      user_two = User.create(email: "a@a.a", password: "aaaaa")
+      user_one = User.create(email: "a@a.ab")
+      user_two = User.create(email: "a@a.ab", password: "aaaaa")
 
       expect(user_one.persisted?).to eq(false)
       expect(user_two.persisted?).to eq(false)
     end
 
     it "requires an email to be unique" do
-      User.create(email: "a@a.a", password: "aaaaaa")
-      user = User.create(email: "a@a.a", password: "aaaaaa")
+      User.create(email: "a@a.ab", password: "aaaaaa")
+      user = User.create(email: "a@a.ab", password: "aaaaaa")
 
       expect(user.persisted?).to eq(false)
     end
 
     it "saves the password securely" do
-      user = User.create(email: "a@a.a", password: "aaaaaa")
+      user = User.create(email: "a@a.ab", password: "aaaaaa")
 
       expect(user.persisted?).to eq(true)
       expect(user.encrypted_password.nil?).to eq(false)
